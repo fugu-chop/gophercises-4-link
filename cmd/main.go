bf7344a7f6d0d52cd75c4beed794c5a7bdd931af
Dean Wan <deanwan14@gmail.com>
2024-03-13 21:10:37 +1100

    refactor: move parsing function to pkg

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
+
+	link "link/pkg"
 
 	"golang.org/x/net/html"
 )
-
-const aTag = "a"
-const href = "href"
-
-type Link struct {
-	Href string
-	Text string
-}
 
 func main() {
 	fileLocationFlag := flag.String(
@@ -38,54 +31,10 @@ func main() {
 		log.Fatalf("could not parse html: %v", err)
 	}
 
-	slice := parseHtml(doc, &[]Link{})
+	slice := link.ParseHtml(doc, &[]link.Link{})
 
 	for _, link := range *slice {
 		fmt.Printf("%+v", link)
 	}
 
-}
-
-func parseHtml(n *html.Node, linkSlice *[]Link) *[]Link {
-	if n.Type == html.ElementNode && n.Data == aTag {
-		for _, a := range n.Attr {
-			if a.Key == href {
-				newLink := Link{
-					Href: a.Val,
-					Text: parseLinkText(n),
-				}
-				*linkSlice = append(*linkSlice, newLink)
-			}
-		}
-	}
-
-	if n.FirstChild != nil {
-		parseHtml(n.FirstChild, linkSlice)
-	}
-
-	if n.NextSibling != nil {
-		parseHtml(n.NextSibling, linkSlice)
-	}
-
-	return linkSlice
-}
-
-func parseLinkText(n *html.Node) string {
-	var linkText string
-
-	if n.Type == html.TextNode && len(n.Data) > 0 {
-		linkText += n.Data
-	}
-
-	if n.FirstChild != nil {
-		linkText += parseLinkText(n.FirstChild)
-	}
-
-	// The additional logic stops the recursion from digging
-	// into neighbouring link tags
-	if n.NextSibling != nil && n.NextSibling.Data != aTag {
-		linkText += parseLinkText(n.NextSibling)
-	}
-
-	return strings.TrimSpace(linkText)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module link
 
 go 1.21.3
 
-require golang.org/x/net v0.22.0 // indirect
+require golang.org/x/net v0.22.0
